2843ba9dfe50d1b810d836076b858afcd73e0092
Ondrashh <xpavla15@vutbr.cz>
2020-10-20 17:04:39 +0200

    Added data creator

---
+++ Nemocnice/DatabaseDataCreator/DatabaseCreatorController.cs
@@ -0,0 +1,22 @@
+﻿using Nemocnice.Data;
+using Nemocnice.DatabaseDataCreator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nemocnice.DatabaseDataCreator
+{
+    public class DatabaseCreatorController
+    {
+        public void DatabaseContentCreate()
+        {
+            using (var db = new DatabaseContext())
+            {
+                LoginCreator login = new LoginCreator();
+                //login.LoginCreate(db);
+            }
+        }
+
+    }
+}
---
+++ Nemocnice/DatabaseDataCreator/LoginCreator.cs
@@ -0,0 +1,24 @@
+﻿using Nemocnice.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nemocnice.DatabaseDataCreator
+{
+    public class LoginCreator
+    {
+        public void LoginCreate(DatabaseContext db)
+        {
+            db.LoginT.Add(LoginAdd("xlogin01","Heslo",1));
+            db.LoginT.Add(LoginAdd("xlogin02", "Heslo1", 5));
+            db.LoginT.Add(LoginAdd("xlogin03", "Heslo2", 3));
+            db.LoginT.Add(LoginAdd("xlogin04", "Heslo3", 2));
+            db.SaveChanges();
+        }
+        private Login LoginAdd(string currentLogin, string hashPassword, int roleId)
+        {
+            return new Login { CurrentLogin = currentLogin, HashPassword = hashPassword, RoleCode = roleId };
+        }
+    }
+}
