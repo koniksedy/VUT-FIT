652fffe4ddb208f0c68b04ef1e7b058d2f69e7b4
Ondrashh <xpavla15@vutbr.cz>
2020-11-12 21:42:56 +0100

    Somehow merged badly

---
+++ Nemocnice/Models/PatientCardModel.cs
@@ -0,0 +1,14 @@
+﻿using Nemocnice.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nemocnice.Models
+{
+    public class PatientCardModel
+    {
+        public User User { get; set; }
+        public Patient Patient { get; set; }
+    }
+}
---
+++ Nemocnice/Models/PatientCheckupModel.cs
@@ -0,0 +1,16 @@
+﻿using Nemocnice.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nemocnice.Models
+{
+    public class PatientCheckupModel
+    {
+        public User User { get; set; }
+        public Patient Patient { get; set; }
+        public List<CheckupTicket> CheckupTicketsDone {get;set;}
+        public List<CheckupTicket> CheckupTicketsRunning { get; set; }
+    }
+}
