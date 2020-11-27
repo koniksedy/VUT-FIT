c4c44b5b5e1c9e1cf2f1916e453a19481eecd04e
Ondrashh <xpavla15@vutbr.cz>
2020-11-27 01:04:20 +0100

    Added some stuf & lot spaces

--- Nemocnice/Controllers/Admin.cs
+++ Nemocnice/Controllers/Admin.cs
@@ -14,6 +14,7 @@ using Nemocnice.Models;
 using X.PagedList.Mvc.Core;
 using X.PagedList;
 using Microsoft.EntityFrameworkCore.Internal;
+using System.Text.RegularExpressions;
 
 namespace Nemocnice.Controllers
 {
@@ -292,25 +293,15 @@ namespace Nemocnice.Controllers
             //pokud je ID_delete různé od 0, víme, jakého doktora chceme odstranit
             if (ID_delete != null)
             {
-
-
-                    var userId = db.DoctorT.Where(x => x.ICZ == int.Parse(ID_delete)).Select(x => x.UserId).FirstOrDefault();
-                    var userLogin = db.UserT.Where(x => x.UserId == userId).Select(x => x.Login).FirstOrDefault();
-                    var user = await _userManager.FindByNameAsync(userLogin);
-                    var kokotconechceodejit = db.DoctorT.Where(a => a.ICZ == int.Parse(ID_delete)).First();
-                    db.DoctorT.Remove(kokotconechceodejit);
-                    db.SaveChanges();
-                    db.UserT.Remove(db.UserT.Where(a => a.UserId == userId).First());
-                    db.SaveChanges();
-                    db.SaveChanges();
-                    db.SaveChanges();
-                    db.SaveChanges();
-                    db.SaveChanges();
-                    if (user != null)
-                    {
-                        await _userManager.DeleteAsync(user);
-                    }
-                    db.SaveChanges();
+                var userId = db.DoctorT.Where(x => x.ICZ == int.Parse(ID_delete)).Select(x => x.UserId).FirstOrDefault();
+                var userLogin = db.UserT.Where(x => x.UserId == userId).Select(x => x.Login).FirstOrDefault();
+                db.UserT.Where(x => x.UserId == userId).First().Deactivated = true;
+                var user = await _userManager.FindByNameAsync(userLogin);
+                if (user != null)
+                {
+                    await _userManager.DeleteAsync(user);
+                }
+                db.SaveChanges();
 
             }
 
@@ -367,5 +358,158 @@ namespace Nemocnice.Controllers
 
             return View(model);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateDoctor()
+        {
+            // Hodnoty pro nového pacienta získané přes POST
+            string name = Request.Form["NewName"];
+            string surname = Request.Form["NewSurname"];
+            string title = Request.Form["NewTitle"];
+            int socialICZ = int.Parse(Request.Form["NewICZ"]);
+            string tel = Request.Form["NewTel"];
+            string email = Request.Form["NewEmail"];
+            string street = Request.Form["NewStreet"];
+            // houseNum může být nezadáno, nejde převést prázdnou hodnotu na int, proto kontroluji.
+            int houseNum = int.Parse(String.IsNullOrEmpty(Request.Form["NewHouseNum"].ToString()) ? "0" : Request.Form["NewHouseNum"].ToString());
+            string city = Request.Form["NewCity"];
+            int zip = int.Parse(Request.Form["NewZip"].ToString().Replace(" ", ""));    // Může být ve tvaru "739 11"
+
+            // Deklarace proměnných pro budoucí vložení do databáze
+            Address address;
+            User user;
+
+            // Kontrola, zda pacient již existuje, pokud ano, nic neděláme.
+            // TODO - vypsat upozornění
+            if (db.DoctorT.Where(o => o.ICZ == socialICZ).ToList().Count != 0)
+            {
+                return RedirectToAction("DoctorEdit");
+            }
+
+            // Vytváření jednotlivých tabulek pro nového pacienta
+            // Po každé vložené tabulce pro jistotu provedu SaveChanges,
+            // protože se hned v zápětí na tuto tabulku dotazuji. (Změny se musí uložit.)
+            // Vytvoření adresy
+            if (houseNum == 0)
+            {
+                address = new Address
+                {
+                    StreetName = street,
+                    City = city,
+                    ZIP = zip
+                };
+            }
+            else
+            {
+                address = new Address
+                {
+                    HouseNumber = houseNum,
+                    StreetName = street,
+                    City = city,
+                    ZIP = zip
+                };
+            }
+            db.AddressT.Add(address);
+            db.SaveChanges();
+
+            string loginsurname = "";
+            if(surname.Length > 7)
+            {
+                loginsurname = surname.Substring(0, 7);
+            }else
+                loginsurname = surname;
+            
+            loginsurname = Regex.Replace(loginsurname, "[éèëêð]", "e");
+            loginsurname = Regex.Replace(loginsurname, "[ÉÈËÊ]", "E");
+            loginsurname = Regex.Replace(loginsurname, "[àâä]", "a");
+            loginsurname = Regex.Replace(loginsurname, "[ÀÁÂÃÄÅ]", "A");
+            loginsurname = Regex.Replace(loginsurname, "[àáâãäå]", "a");
+            loginsurname = Regex.Replace(loginsurname, "[ÙÚÛÜ]", "U");
+            loginsurname = Regex.Replace(loginsurname, "[ùúûüµ]", "u");
+            loginsurname = Regex.Replace(loginsurname, "[òóôõöø]", "o");
+            loginsurname = Regex.Replace(loginsurname, "[ÒÓÔÕÖØ]", "O");
+            loginsurname = Regex.Replace(loginsurname, "[ìíîï]", "i");
+            loginsurname = Regex.Replace(loginsurname, "[ÌÍÎÏ]", "I");
+            loginsurname = Regex.Replace(loginsurname, "[č]", "c");
+            loginsurname = Regex.Replace(loginsurname, "[ř]", "r");
+            loginsurname = Regex.Replace(loginsurname, "[ž]", "z");
+            loginsurname = Regex.Replace(loginsurname, "[ý]", "y");
+            loginsurname = Regex.Replace(loginsurname, "[í]", "i");
+            loginsurname = Regex.Replace(loginsurname, "[ů]", "u");
+            loginsurname = Regex.Replace(loginsurname, "[ú]", "u");
+            loginsurname = Regex.Replace(loginsurname, "[ě]", "e");
+            loginsurname = Regex.Replace(loginsurname, "[ť]", "t");
+            loginsurname = Regex.Replace(loginsurname, "[ň]", "n");
+            loginsurname = Regex.Replace(loginsurname, "[ď]", "d");
+            loginsurname = Regex.Replace(loginsurname, "[š]", "s");
+            loginsurname = Regex.Replace(loginsurname, "[Š]", "S");
+            loginsurname = Regex.Replace(loginsurname, "[ñ]", "n");
+            loginsurname = Regex.Replace(loginsurname, "[Ñ]", "N");
+            loginsurname = Regex.Replace(loginsurname, "[ç]", "c");
+            loginsurname = Regex.Replace(loginsurname, "[Ç]", "C");
+            loginsurname = Regex.Replace(loginsurname, "[ÿý]", "y");
+            loginsurname = Regex.Replace(loginsurname, "[Ÿ]", "Y");
+            loginsurname = Regex.Replace(loginsurname, "[ž]", "z");
+            loginsurname = Regex.Replace(loginsurname, "[Ž]", "Z");
+            loginsurname = Regex.Replace(loginsurname, "[Ð]", "D");
+            loginsurname = Regex.Replace(loginsurname, "[œ]", "oe");
+            loginsurname = Regex.Replace(loginsurname, "[Œ]", "Oe");
+            loginsurname = Regex.Replace(loginsurname, "[«»\u201C\u201D\u201E\u201F\u2033\u2036]", "\"");
+            loginsurname = Regex.Replace(loginsurname, "[\u2026]", "...");
+            loginsurname = loginsurname.ToLower();
+            int suffix = 0;
+            string loginCreator = "x" + loginsurname + String.Format("{0:D2}",suffix);
+            List<string> logins = db.UserT.Select(x => x.Login).ToList();
+            if (logins.Contains(loginCreator) == true)
+            {
+                suffix++;
+                loginCreator = "x" + surname + String.Format("{0:D2}", suffix);
+            }
+
+
+
+            // Vytvoření uživatele (Jméno, Příjmení, Titul, ...)
+            // Uživatelským jménem pacientů je jejich rodné číslo.
+            user = new User
+            {
+                Login = loginCreator,
+                Name = name,
+                Phone = tel,
+                Surname = surname,
+                Title = title,
+                Email = email,
+                WorkAddress = new Address
+                {
+                    City = city,
+                    StreetName = street,
+                    HouseNumber = houseNum,
+                    ZIP = zip
+                }
+            };
+            db.UserT.Add(user);
+            db.SaveChanges();
+
+            // Vytvoření pacienta.
+            // Napojení Address a User
+            db.DoctorT.Add(new Data.Doctor
+            {
+                UserId = db.UserT.Where(o => o.Login == user.Login).Select(s => s.UserId).ToList().First(),
+                ICZ = socialICZ,
+                WorkPhone = tel
+            });
+            db.SaveChanges();
+
+            // Vytvoření nového uživatele pro Identity Framework.
+            // Základní heslo je 1234567890, uživatelské jméno je shodné s rodným číslem.
+            // Udělení oprávnění Patient.
+            var userIdentity = new NemocniceUser { UserName = user.Login };
+            var result = await _userManager.CreateAsync(userIdentity, "1234567890");
+            await _userManager.AddToRoleAsync(userIdentity, "Doctor");
+            db.SaveChanges();
+            // Návrat zpět do kartotéky.
+            return RedirectToAction("DoctorEdit", new { SortOrder = Request.Form["SortOrder"], p = Request.Form["p"], Search = Request.Form["Search"] });
+        }
     }
+
+
 }
