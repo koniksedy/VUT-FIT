eb2264ce3058ba15978bd46b87faf391ddd8e078
koniksedy <xsedym02@vutbr.cz>
2022-10-19 01:30:22 +0200

    find works

--- src/find.py
+++ src/find.py
@@ -1,97 +1,110 @@
 from cisjr import database_api
 
 
-db = database_api.Database("cisjr", "mongodb://localhost:27017")
+def find(from_st: str, to_st: str, date: str):
 
-from_st = "Praha hl. n."
-to_st = "Praha-Holešovice"
-date = "20221018"
+    db = database_api.Database("cisjr", "mongodb://localhost:27017")
 
-cursor = db.api_aggregate("CZPTTCISMessage", [
-    {
-        "$match": {
-            "CZPTTInformation.CZPTTLocation.Location.Name": from_st
-        }
-    },
-    {
-        "$match": {
-            "CZPTTInformation.CZPTTLocation.Location.Name": to_st
-        }
-    },
-    {
-        "$match": {
-            "CZPTTInformation.PlannedCalendar.Valid": date
-        }
-    },
-    {"$match": {
-            "$expr": {
-                "$eq": [
-                    {
-                        "$let": {
-                            "vars": {
-                                "a": {
-                                    "$arrayElemAt": [
-                                        {
-                                            "$filter": {
-                                                "input": "$CZPTTInformation.CZPTTLocation",
-                                                "cond": {"$and": [{"$eq": ["$$this.Location.Name", from_st]}, {"$eq": ["$$this.TrainActivity", "0001"]}]}
-                                            }
-                                        },
-                                        0
-                                    ]
-                                },
-                                "b": {
-                                    "$arrayElemAt": [
-                                        {
-                                            "$filter": {
-                                                "input": "$CZPTTInformation.CZPTTLocation",
-                                                "cond": {"$and": [{ "$eq": ["$$this.Location.Name", to_st]}, {"$eq": ["$$this.TrainActivity", "0033"]}]}
-                                            }
-                                        },
-                                        0
-                                    ]
-                                }
+    cursor = db.api_aggregate("CZPTTCISMessage", [
+        {
+            "$match": {
+                "CZPTTInformation.CZPTTLocation.Location.Name": from_st
+            }
+        },
+        {
+            "$match": {
+                "CZPTTInformation.CZPTTLocation.Location.Name": to_st
+            }
+        },
+        {
+            "$match": {
+                "CZPTTInformation.PlannedCalendar.Valid": date
+            }
+        },
+        {
+            "$match": {
+                "$expr": {
+                    "$lt": [
+                        {
+                            "$indexOfArray": [
+                                "$CZPTTInformation.CZPTTLocation.Location.Name", from_st
+                            ]
+                        },
+                        {
+                            "$indexOfArray": [
+                                "$CZPTTInformation.CZPTTLocation.Location.Name", to_st
+                            ]
+                        }
+                    ]
+                }
+            }
+        },
+        {
+            "$project": {
+                "_id": 0,
+                "Locations": {
+                    "$let": {
+                        "vars": {
+                            "a": {
+                                "$indexOfArray": [
+                                    "$CZPTTInformation.CZPTTLocation.Location.Name", from_st
+                                ]
                             },
-                            "in": {
-                                "$or": [
-                                    {
-                                        "$lt": ["$$a.TimingAtLocation.ALD.Time", "$$b.TimingAtLocation.ALA.Time"]
-                                    },
-                                    {
-                                        "$lt": ["$$a.TimingAtLocation.ALD.Offset", "$$b.TimingAtLocation.ALA.Offset"]
-                                    }
+                            "b": {
+                                "$indexOfArray": [
+                                    "$CZPTTInformation.CZPTTLocation.Location.Name", to_st
                                 ]
                             }
+                        },
+                        "in": {
+                            "$slice" : [
+                                {
+                                    "$map": {
+                                        "input": "$CZPTTInformation.CZPTTLocation",
+                                        "as": "location",
+                                        "in": {
+                                            "Name": "$$location.Location.Name",
+                                            "ALD":  {
+                                                "$dateToString": {
+                                                    "format": "%H:%M:%S",
+                                                    "date": "$$location.TimingAtLocation.ALD.Time"
+                                                }
+                                            },
+                                            "ALA": {
+                                                "$dateToString": {
+                                                    "format": "%H:%M:%S",
+                                                    "date": "$$location.TimingAtLocation.ALA.Time"
+                                                }
+                                            }
+                                        }
+                                    }
+                                },
+                                "$$a", {"$subtract": [{"$add": [1, "$$b"]}, "$$a"]}
+                            ]
                         }
-                    },
-                    True
-                ]
+                    }
+                }
             }
         }
-    },
-    {
-        "$project": {
-            # "_id": 1
-            "CZPTTInformation.CZPTTLocation.Location.Name": 1,
-            "CZPTTInformation.CZPTTLocation.TimingAtLocation.ALD.Time": 1
-        }
-    }
-])
-total_l = list(cursor)
-l = total_l[0]
-praha = list()
-for l in total_l:
-    for i in range(len(l["CZPTTInformation"]["CZPTTLocation"])):
-        if l["CZPTTInformation"]["CZPTTLocation"][i]["Location"]["Name"] == "Praha hl. n.":
-            praha.append((l["CZPTTInformation"]["CZPTTLocation"][i]["TimingAtLocation"]["ALD"]["Time"], l["_id"]))
-        print(i, l["CZPTTInformation"]["CZPTTLocation"][i]["Location"]["Name"], end=" ")
-        if "ALD" in l["CZPTTInformation"]["CZPTTLocation"][i]["TimingAtLocation"]:
-            print(l["CZPTTInformation"]["CZPTTLocation"][i]["TimingAtLocation"]["ALD"]["Time"])
-        else:
-            print()
-    print("-"*60)
-for t in sorted(praha, key=lambda x: x[0]):
-    print(t)
-print(len(total_l))
+    ])
 
-db.close()
+    data = list(cursor)
+    db.close()
+
+    return data
+
+
+def main():
+    from_st = "Praha hl. n."
+    to_st = "Pardubice hl. n."
+    date = "20221018"
+
+    total_l = find(from_st, to_st, date)
+    for l in total_l:
+        for t in l["Locations"]:
+            print(t)
+        print("-"*80)
+
+
+if __name__ == "__main__":
+    main()
