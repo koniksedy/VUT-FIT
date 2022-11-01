d33e75bb03573b014d66079340f725e98ecfc049
koniksedy <xsedym02@vutbr.cz>
2022-11-01 12:28:13 +0100

    DiGraph finished

--- src/gal/digraph.py
+++ src/gal/digraph.py
@@ -1,9 +1,9 @@
 """
-parser.py
-DiGraph parsing module.
+digraph.py
+DiGraph module.
 Authors: Bc. Jan Bíl
          Bc. Michal Šedý
-Last change: 31.10.2022
+Last change: 01.11.2022
 """
 
 import numpy as np
@@ -15,7 +15,7 @@ class DiGraph:
     Attributes:
         vertices (set[int]): A set of renamed (id of the vertex) graph states.
         vertices_cnt (int): Cont of graph vertices.
-        vertex_cname (bidict): Two way dictionary with id (key) of vertex and original name (value).
+        vertex_cname (bidict): Two-way dictionary with id (key) of vertex and original name (value).
         edges_mx (np.array): Two dimensional boolean incident matrix.
     """
     def __init__(self) -> None:
@@ -43,6 +43,17 @@ class DiGraph:
     def __repr__(self) -> str:
         return str(self.edges_mx)
 
+    def set_vertices(self, vertices: set) -> None:
+        """Prepare graph for given vertices. The previous graph data will be deleted.
+
+        Args:
+            vertices (set): Vertices
+        """
+        self.vertices_cnt = len(vertices)
+        self.vertices = set(range(self.vertices_cnt))
+        self.vertex_cname = bidict({i: v for i, v in zip(self.vertices, vertices)})
+        self.edges_mx = np.zeros((self.vertices_cnt, self.vertices_cnt), dtype=bool)
+
     def add_vertex(self, vertex: str) -> None:
         """Adds new vertex to the graph.
 
@@ -51,9 +62,9 @@ class DiGraph:
         """
         if vertex not in self.vertex_cname.inverse:
             # Add vertex
-            vertex_id = self.vertices_cnt
-            self.vertex_cname[vertex_id] = vertex
-            self.vertices.add(vertex_id)
+            v_id = self.vertices_cnt
+            self.vertex_cname[v_id] = vertex
+            self.vertices.add(v_id)
             self.vertices_cnt += 1
 
             # Add new row and column to incident matrix.
@@ -80,6 +91,19 @@ class DiGraph:
 
         if not self.edges_mx[src][dst]:
             self.edges_mx[src][dst] = True
+
+    def transpose(self):
+        """Creates transposed graph.
+
+        Returns:
+            DiGraph: Transposed graph.
+        """
+        graph = DiGraph()
+        graph.vertices = self.vertices
+        graph.vertices_cnt = self.vertices_cnt
+        graph.vertex_cname = self.vertex_cname
+        graph.edges_mx = self.edges_mx.transpose()
+        return graph
 
     def prune_single_scc(self) -> None:
         """Recursively removes vertices with 0 input degree or 0 output degree.
@@ -112,7 +136,7 @@ class DiGraph:
         self.vertices = set(range(self.vertices_cnt))
         new_canon = bidict()
         i = 0
-        for v_id in self.vertex_cname:
+        for v_id in sorted(self.vertex_cname.keys()):
             if v_id not in pruned:
                 new_canon[i] = self.vertex_cname[v_id]
                 i += 1
@@ -129,3 +153,86 @@ class DiGraph:
             np.array: Successors of the vertex.
         """
         return np.argwhere(self.edges_mx[vertex_id] == True).flatten()
+
+    def get_induced_graph(self, vertices: set):
+        """Creates induced graph based on the set of given vertices.
+
+        Args:
+            vertices (set): Vertices of the new graph.
+
+        Returns:
+            DiGraph: Induced graph.
+        """
+        graph = DiGraph()
+        if not vertices:
+            return graph
+
+        graph.vertices_cnt = len(vertices)
+        graph.vertices = set(range(graph.vertices_cnt))
+        graph.edges_mx = np.zeros((graph.vertices_cnt, graph.vertices_cnt), dtype=bool)
+
+        # Rebase edges and cname
+        new_ids = {v_id: new_id for v_id, new_id in zip(vertices, graph.vertices)}
+        for u in vertices:
+            graph.vertex_cname[new_ids[u]] = self.vertex_cname[u]
+            for v in vertices:
+                graph.edges_mx[new_ids[u]][new_ids[v]] = self.edges_mx[u][v]
+
+        return graph
+
+    def dfs(self, order_key = None, order_reverse = False) -> tuple:
+        """Performs DFS.
+
+        Args:
+            order_key (function, optional): Function according to which the root vertices are selected
+                                            (in the main for cycle). If None, then vertices are chosen
+                                            randomly. Defaults to None.
+            order_reverse (bool, optional): If True, then the root vertices are selected in
+                                            descending order. Otherwise, the ascending order is applied.
+                                            Defaults to False.
+
+        Returns:
+            tuple[list, list, list]: The method returns the list of trees (a set of vertices) in the DFS
+                                     forest, the list of d times (times of the beginning of a work on
+                                     vertices), and the list of f times (times of the end of a work on vertices).
+        """
+        def dfs_visit(s) -> set:
+            nonlocal time
+            visited = {s}
+            colors[s] = GRAY
+            time += 1
+            d[s] = time
+            for v in self.get_successors(s):
+                if colors[v] == WHITE:
+                    visited.update(dfs_visit(v))
+            colors[s] = BLACK
+            time += 1
+            f[s] = time
+            return visited
+
+        WHITE = 0
+        GRAY = 1
+        BLACK = 2
+
+        trees_vertices = list()
+        colors = [WHITE] * self.vertices_cnt
+        time = 0
+        d = [0] * self.vertices_cnt
+        f = [0] * self.vertices_cnt
+
+        for u in sorted(self.vertices, key=order_key, reverse=order_reverse):
+            if colors[u] == WHITE:
+                trees_vertices.append(dfs_visit(u))
+
+        return trees_vertices, d, f
+
+    def get_scc(self) -> list:
+        """Creates SCC of the graph.
+
+        Returns:
+            list[DiGraph]: List of SCC.
+        """
+        _, _, f1 = self.dfs()
+        graph_t = self.transpose()
+        trees_vertices, _, _ = graph_t.dfs(order_key=lambda x: f1[x], order_reverse=True)
+        return [self.get_induced_graph(tree_v) for tree_v in trees_vertices]
