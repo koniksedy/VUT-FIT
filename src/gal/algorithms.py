f5af0f66b097200727296a49d4402239ad52ec61
Jan Bíl <bil.j@seznam.cz>
2022-11-01 22:03:31 +0100

    Added book algorithm

--- src/gal/algorithms.py
+++ src/gal/algorithms.py
@@ -8,6 +8,7 @@ Last change: 01.11.2022
 
 from queue import Queue
 import networkx as nx
+import numpy as np
 from . import digraph
 
 def __get_cycles_nx(graph) -> list():
@@ -82,6 +83,44 @@ def __get_cycles_hj(input_graph: digraph.DiGraph) -> list():
 
     return cycles
 
+def __get_cycles_book(input_graph: digraph.DiGraph) -> list():
+    graph = digraph.DiGraph(input_graph)
+    graph.prune_single_scc()
+    cycles = list()
+    for vertex in graph.vertices:
+        #Find paths starting from each vertex
+        curr_vertex = vertex
+        H =  np.zeros((graph.vertices_cnt, graph.vertices_cnt), dtype=bool)
+        curr_path = list()
+        while curr_vertex >= 0:
+            found = False
+            for succ in graph.get_successors(curr_vertex):
+                if H[curr_vertex][succ] == 0 and (succ not in curr_path or succ == vertex) and succ >= vertex:
+                    #Take first available edge
+                    curr_path.append(curr_vertex)
+                    found = True
+                    if succ == vertex:
+                        #If cycle is found, add it to list and forbid this edge from further search
+                        H[curr_path[-1]][succ] = True
+                        temp_path = list(curr_path)
+                        temp_path.append(succ)
+                        cycles.append(temp_path)
+                        curr_vertex = curr_path.pop(-1)
+                    else:
+                        curr_vertex = succ
+                    break
+            if not found:
+                # If no available edge is found, go back one step, forbid previously taken edge and continue
+                if not curr_path:
+                    # If no available edge from first vertex, break the while loop
+                    curr_vertex = -1
+                    continue
+                H[curr_path[-1]][curr_vertex] = True
+                curr_vertex = curr_path.pop(-1)
+    return cycles
+
+
+
 def get_cycles(input_graph: digraph.DiGraph, algo="nx") -> list:
     """Enumerates all cycles (elementary circuits) in a graph.
 
@@ -98,5 +137,7 @@ def get_cycles(input_graph: digraph.DiGraph, algo="nx") -> list:
         return __get_cycles_nx(input_graph)
     elif algo == "hj":
         return __get_cycles_hj(input_graph)
+    elif algo == "book":
+        return __get_cycles_book(input_graph)
     else:
         raise ValueError("Unknown algorithm name.")
