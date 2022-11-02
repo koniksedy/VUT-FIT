56196ee6f5086c71d76888991739454c5dc4815d
koniksedy <xsedym02@vutbr.cz>
2022-11-02 19:54:35 +0100

    work in progress

--- src/gal/algorithms.py
+++ src/gal/algorithms.py
@@ -7,6 +7,7 @@ Last change: 02.11.2022
 """
 
 import networkx as nx
+import numpy as np
 from queue import Queue
 from . import digraph
 
@@ -71,12 +72,108 @@ def __get_cycles_hj(input_graph: digraph.DiGraph) -> list():
         for v in graph.edges[last]:
             if v == head:
                 # Report cycle.
-                cycles.append(path)
+                # Translace local vertex ids from scc to their ids from input_graph.
+                cycles.append([input_graph.vertex_cname.inv[graph.vertex_cname[v]] for v in path])
             elif v > min_path:
                 if v not in path:
                     q.put(path + [v])
 
     return cycles
+
+def __get_cycle_wein(input_graph: digraph.DiGraph):
+    def concat(is_recursive: bool, TT: list, P: list) -> None:
+        def get_tail(L: list, item) -> list:
+            t = list()
+            found = False
+            for v in L:
+                if found:
+                    t.append(v)
+                if v == item:
+                    found = True
+            return t
+
+        cycle_tails = list()
+        to_add = list()
+
+        v = P[-1]
+        for c in cycles:
+            tail = get_tail(c, v)
+            if not tail or tail in cycle_tails:
+                continue
+            cycle_tails.append(tail)
+
+            if set(tail).intersection(P):
+                continue
+
+            if S_v[c[-1]] == 2:
+                to_add.extend(concat(True, TT, P + tail))
+                continue
+            else:
+                assert S_v[[c[-1]]] == 1
+                new_c = [c[-1]] + get_tail(TT, c[-1]) + P + get_tail(c, P[-1])
+                if is_recursive:
+                    cycles.append(new_c)
+                else:
+                    to_add.append(new_c)
+
+        if is_recursive:
+            for c in set(to_add):
+                cycles.append(c)
+
+        return to_add
+
+    def examine(TT: list, v: int) -> None:
+        if S_v[v] == 0:
+            S_v[v] = 1
+            # go back to extend
+        elif S_v[v] == 1:
+            cycles.append(TT)
+            TT.pop(-1)
+            # go back to extend and if TT is empty, than go to main funciton
+        else:
+            concat(False, TT, [v])
+            TT.pop(-1)
+            # go back to extend and if TT is empty, than go to main funciton
+
+    def get_suitable_vertex(TT: list):
+        u = TT[-1]
+        for v in graph.edges[u]:
+            if S_e[u][v] == 0:
+                return v
+
+        return None
+
+    def extend(TT: list) -> None:
+        while TT:
+            u = TT[-1]
+            v = get_suitable_vertex(TT)
+            if v is None:
+                S_v[u] = 2
+                TT.pop(-1)
+            else:
+                S_e[u][v] = 2
+                TT.append(v)
+                examine(TT, v)
+
+    # Simplify graph
+    graph = digraph.DiGraph(input_graph)
+    graph.prune_single_scc()
+
+    cycles = list()
+
+    # Init
+    S_v = [0] * graph.vertices_cnt
+    S_e = np.zeros((graph.vertices_cnt, graph.vertices_cnt), dtype=int)
+    TT = list()
+
+    # Calculation
+    TT = []
+    for v in graph.vertices:
+        if S_v[v] == 0:
+            S_v[v] = 1
+            TT = [v]
+            extend(TT)
+
 
 def get_cycles(input_graph: digraph.DiGraph, algo="nx") -> list:
     """Enumerates all cycles (elementary circuits) in a graph.
@@ -86,6 +183,7 @@ def get_cycles(input_graph: digraph.DiGraph, algo="nx") -> list:
         algo (str): A type of algorithm to be used.
                     nx - using networkx library
                     hj - using Hongbo Liu and Jiaxin Wang algorithm
+                    wein - using Weinblatt's algorithm
 
     Returns:
         list: List of cycles.
@@ -94,5 +192,7 @@ def get_cycles(input_graph: digraph.DiGraph, algo="nx") -> list:
         return __get_cycles_nx(input_graph)
     elif algo == "hj":
         return __get_cycles_hj(input_graph)
+    elif algo == "wein":
+        return __get_cycle_wein(input_graph)
     else:
         raise ValueError("Unknown algorithm name.")
