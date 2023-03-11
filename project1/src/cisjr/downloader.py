d515613abd042c63948905fe0c91f7179e8c79e0
koniksedy <xsedym02@vutbr.cz>
2023-03-11 22:31:49 +0100

    readd project 1 and 2

---
+++ project1/src/cisjr/downloader.py
@@ -0,0 +1,238 @@
+"""
+downloader.py
+Executable module for data downloading from cisjr.
+Data can be downloaded in parallel.
+UPA project
+Authors: Bc. Martina Chripková <xchrip01@stud.fit.vutbr.cz>
+         Bc. Martin Novotný Mlinárcsik <xnovot1r@stud.fit.vutbr.cz>
+         Bc. Michal Šedý <xsedym02@stud.fit.vutbr.cz>
+Last change: 19.11.2022
+"""
+
+import os
+import sys
+import bs4
+import shutil
+import tempfile
+import pathlib
+import tqdm
+import gzip
+import zipfile
+import requests
+import urllib.request
+from . import database_api
+
+
+class Downloader:
+    def __init__(self, url: str, workdir: str = None) -> None:
+        self.url = url
+        self.portal = url.split("://")[0] + "://" + url.split("://")[1].split("/")[0]
+
+        if workdir is None:
+            self.workdir = tempfile.mkdtemp(prefix="cisjr")
+            # If set to true, the data folder will be deleted.
+            self._clear_at_exit = True
+        else:
+            pathlib.Path(workdir).mkdir(parents=True, exist_ok=True)
+            self.workdir = workdir
+            # If set to true, the data folder will be deleted.
+            self._clear_at_exit = False
+
+        self.folder_zip = f"{self.workdir}/data"
+        if not os.path.exists(self.folder_zip):
+            os.mkdir(self.folder_zip)
+        self.folder_xml = f"{self.workdir}/zips"
+        if not os.path.exists(self.folder_xml):
+            os.mkdir(self.folder_xml)
+
+        self.process_bar = None
+
+    def __del__(self) -> None:
+        if self._clear_at_exit:
+            shutil.rmtree(self.workdir)
+
+    @staticmethod
+    def _get_files(dir: str) -> list:
+        """Iterates recursively over directory and returns all founded file paths.
+
+        Args:
+            dir (str): Directory.
+
+        Returns:
+            list: Paths of the files in the dir.
+        """
+        files = list()
+        for name in os.listdir(dir):
+            full_name = f"{dir}/{name}"
+            if os.path.isfile(full_name):
+                files.append(full_name)
+            else:
+                files.extend(Downloader._get_files(full_name))
+        return files
+
+    def _get_links(self, url: str) -> set:
+        """Finds all links on a page given by url.
+
+        Args:
+            url (str): Url of the page.
+        Returns:
+            set: A set of links.
+        """
+        links = set()
+        rqst = requests.get(url).content
+        bs = bs4.BeautifulSoup(rqst, "html.parser")
+        content = bs.find_all("a", href=True)
+        for link in content:
+            if link["href"] in url:
+                continue
+            if link["href"].endswith("/"):
+                links.update(self._get_links(self.portal + link["href"]))
+            else:
+                links.add(link["href"])
+
+        return links
+
+    def _unzip(self, files: set) -> None:
+        """Unzips all given files from folder self.folder_zip
+        and saves them into the folder self.folder_xml with
+        the same directory hierarchy.
+
+        Args:
+            files (set): Name of the files to be unzipped.
+        """
+        for file_name in files:
+            out_folder = f"{self.folder_xml}/{'/'.join(file_name.split('/')[:-1])}"
+            pathlib.Path(out_folder).mkdir(parents=True, exist_ok=True)
+            if file_name.endswith(".xml.zip"):
+                # Zips can be of two different types, gzip or zip.
+                # It is impossible to determine the exact type, therefore you need to try it.
+                try:
+                    out_name = f"{file_name.split('/')[-1][:-8]}.xml"
+                    with gzip.open(f"{self.folder_zip}/{file_name}", "rb") as fd_in:
+                        with open(f"{out_folder}/{out_name}", 'wb') as fd_out:
+                            shutil.copyfileobj(fd_in, fd_out)
+                except gzip.BadGzipFile:
+                    with zipfile.ZipFile(f"{self.folder_zip}/{file_name}", "r") as zip_d:
+                        zip_d.extractall(out_folder)
+            else:
+                with zipfile.ZipFile(f"{self.folder_zip}/{file_name}", "r") as zip_d:
+                        zip_d.extractall(out_folder)
+
+    def _parallel_download(self, link: list) -> None:
+        file_name = link.split("/")[-1]
+        folder_name = "/".join(link[32:].split("/")[:-1])
+        if not os.path.exists(folder_name):
+            pathlib.Path(f"{self.folder_zip}/{folder_name}").mkdir(parents=True, exist_ok=True)
+            urllib.request.urlretrieve(self.portal + link, f"{self.folder_zip}/{folder_name}/{file_name}")
+
+    def download(self, except_links: set = set(), n_threads: int = 1) -> set:
+        """The method downloads and extracts *.xml.zip data from self.url.
+        Raw data are in the folder specified by self.folder_zip.
+        Extracted files are in the folder specified by self.folder_xml.
+
+        Args:
+            except_links (set, optional): A set of links that are already downloaded.
+                                          Defaults to the empty set.
+            n_threads (int, optional): If greater than 1, the parallel downloading with
+                                       n_threads is performed. Defaults 1.
+        Returns:
+            set: A of set of links of downloaded files.
+        """
+        # Get remote file links
+        print("Initializing... ", file=sys.stderr, end="")
+        sys.stderr.flush()
+        downloaded_links = self._get_links(self.url).difference(except_links)
+        print("Done", file=sys.stderr)
+
+        # Downloading (can be done in parallel)
+        if n_threads > 1:
+            from pqdm.processes import pqdm
+            print("Parallel downloading...", file=sys.stderr)
+            pqdm(downloaded_links, self._parallel_download, n_jobs=n_threads)
+            print("Downloading done.", file=sys.stderr)
+        else:
+            for link in tqdm.tqdm(downloaded_links,
+                                desc="Downloading...",
+                                ascii=False,
+                                file=sys.stderr):
+                file_name = link.split("/")[-1]
+                folder_name = "/".join(link[32:].split("/")[:-1])
+                if not os.path.exists(folder_name):
+                    pathlib.Path(f"{self.folder_zip}/{folder_name}").mkdir(parents=True, exist_ok=True)
+                    urllib.request.urlretrieve(self.portal + link, f"{self.folder_zip}/{folder_name}/{file_name}")
+
+        # Unzip
+        zip_paths = set(map(lambda x: x[32:], downloaded_links))
+        originals = set(filter(lambda x: "oprava" not in x, zip_paths))
+        updates = zip_paths.difference(originals)
+
+        print("Unzipping originals... ", end="", file=sys.stderr)
+        sys.stderr.flush()
+        self._unzip(originals)
+        print("Done", file=sys.stderr)
+
+        print("Unzipping updates... ", end="", file=sys.stderr)
+        sys.stderr.flush()
+        self._unzip(updates)
+        print("Done", file=sys.stderr)
+
+        return downloaded_links
+
+    def get(self) -> list:
+        """Returns sorted list of paths to xml files from self.folder_xml.
+
+        Returns:
+            list: Sorted list of paths to xml files.
+        """
+        return list(sorted(filter(lambda x: x.endswith(".xml"), Downloader._get_files(self.folder_xml))))
+
+    def set_clear_at_exit(self):
+        # If set to true, the data folder will be deleted.
+        self._clear_at_exit = True
+
+
+def main():
+    from argparse import ArgumentParser
+
+    # Parse args
+    opt_parser = ArgumentParser()
+    opt_parser.add_argument("-p", "--parallel", type=int, metavar="N_THR", default=1,
+                            help=("Download data in 0 < N_THREADS parallel threads.\n" +
+                                  "WARNING high number can lead to the lag in QUEUEING TASKS\n" +
+                                  "Workdir must be specified."))
+    opt_parser.add_argument("-f", "--force", action="store_true",
+                            help="Downloads already downloaded files.")
+    opt_parser.add_argument("workdir", nargs="?",
+                            help="Name of a working directory. (default /tmp).")
+    args = opt_parser.parse_args()
+
+    if args.parallel > 1 and args.workdir is None:
+        print("A workdir must be specified when parallelizing.", file=sys.stderr)
+        sys.exit(1)
+
+    # Connect to DB
+    connection_string = f"mongodb://localhost:27017"
+    db = database_api.Database("cisjr", connection_string)
+
+    # Get existing links
+    except_links = set()
+    if not args.force:
+        for link in db.api_find("Downloaded"):
+            except_links.add(link["Link"])
+    else:
+        db.api_drop_collection("Downloaded")
+
+    # Downloading
+    down = Downloader("https://portal.cisjr.cz/pub/draha/celostatni/szdc/2022/", workdir=args.workdir)
+    downloaded_links = down.download(except_links=except_links, n_threads=args.parallel)
+
+    # Save downloaded links to DB
+    if downloaded_links:
+        db.api_insert_many("Downloaded", [{"Link": link} for link in downloaded_links])
+
+    # Close db (Database Class) client member
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ project1/src/cisjr/parser.py
@@ -0,0 +1,329 @@
+"""
+parser.py
+Module parses xml data.
+UPA project
+Authors: Bc. Martina Chripková <xchrip01@stud.fit.vutbr.cz>
+         Bc. Martin Novotný Mlinárcsik <xnovot1r@stud.fit.vutbr.cz>
+         Bc. Michal Šedý <xsedym02@stud.fit.vutbr.cz>
+Last change: 18.10.2022
+"""
+
+import sys
+import tqdm
+import xml.etree.ElementTree as ET
+from datetime import datetime, timedelta
+
+
+class Parser:
+    def __init__(self) -> None:
+        self.Locations_dict = dict()
+
+    def parse_single(self, xml_file: str) -> tuple:
+        """Parses given xml file according to CIS structure.
+        And returns a tuple of lists with parsed objects.
+
+        Args:
+            xml_file (str): xml file to parse.
+
+        Raises:
+            ValueError: Unsupported tag. Just for testing.
+
+        Returns:
+            tuple: Return triple of elements.
+                   CZPTTCISMessage, CZCanceledPTTMessage, Locations
+        """
+        CZPTTCISMessage = None
+        CZCanceledPTTMessage = None
+
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        if root.tag == "CZPTTCISMessage":
+            message = self._parse_CZPTTCISMessage(root)
+            CZPTTCISMessage = message
+        elif root.tag == "CZCanceledPTTMessage":
+            message = self._parse_CZCanceledPTTMessage(root)
+            CZCanceledPTTMessage = message
+
+        Locations = list(self.Locations_dict.items()) if self.Locations_dict else None
+
+        return CZPTTCISMessage, CZCanceledPTTMessage, Locations
+
+    def parse(self, xml_files: list) -> tuple:
+        """Parses given xml files according to CIS structure.
+        And returns a tuple of lists with parsed objects.
+
+        Args:
+            xml_files (list): xml file to parse.
+
+        Raises:
+            ValueError: Unsupported tag. Just for testing.
+
+        Returns:
+            Tuple[list, list, list]: Return triple of lists.
+                                     CZPTTCISMessage, CZCanceledPTTMessage, Locations
+        """
+        CZPTTCISMessage_list = list()
+        CZCanceledPTTMessage_list = list()
+
+        for xml_f in tqdm.tqdm(xml_files,
+                               desc="Parsing...",
+                               ascii=False,
+                               file=sys.stderr):
+            tree = ET.parse(xml_f)
+            root = tree.getroot()
+            if root.tag == "CZPTTCISMessage":
+                message = self._parse_CZPTTCISMessage(root)
+                CZPTTCISMessage_list.append(message)
+            elif root.tag == "CZCanceledPTTMessage":
+                message = self._parse_CZCanceledPTTMessage(root)
+                CZCanceledPTTMessage_list.append(message)
+
+        Locations = list(self.Locations_dict.items())
+
+        return CZPTTCISMessage_list, CZCanceledPTTMessage_list, Locations
+
+    def _parse_CZPTTCISMessage(self, xml_root: ET.Element) -> dict:
+        out = dict()
+        for child in xml_root:
+            if child.tag == "Identifiers":
+                out["Identifiers"] = self._parse_Identifiers(child)
+            elif child.tag == "CZPTTCreation":
+                out["CZPTTCreation"] = self._parse_date_time(child.text)
+            elif child.tag == "CZPTTHeader":
+                out["CZPTTHeader"] = self._parse_CZPTTHeader(child)
+            elif child.tag == "CZPTTInformation":
+                assert "CZPTTInformation" not in out
+                out["CZPTTInformation"] = self._parse_CZPTTInformation(child)
+            elif child.tag == "NetworkSpecificParameter":
+                if "NetworkSpecificParameter" not in out:
+                    out["NetworkSpecificParameter"] = list()
+                out["NetworkSpecificParameter"].append(self._parse_NetworkSpecificParameter(child))
+
+        return out
+
+    def _parse_CZCanceledPTTMessage(self, xml_root: ET.Element) -> dict:
+        out = dict()
+        for child in xml_root:
+            if child.tag == "PlannedTransportIdentifiers":
+                if "PlannedTransportIdentifiers" not in out:
+                    out["PlannedTransportIdentifiers"] = dict()
+                key, val = self._parse_TransportIdentifiers(child)
+                out["PlannedTransportIdentifiers"][key] = val
+            elif child.tag == "CZPTTCancelation":
+                out["CZPTTCancelation"] = self._parse_date_time(child.text)
+            elif child.tag == "CZDeactivatedSection":
+                out["CZDeactivatedSection"] = self._parse_CZDeactivatedSection(child)
+            elif child.tag == "PlannedCalendar":
+                out["PlannedCalendar"] = self._parse_PlannedCalendar(child)
+
+        return out
+
+    def _parse_CZPTTHeader(self, xml_root: ET.Element) -> dict:
+        out = dict()
+        for child in xml_root:
+            if child.tag == "CZForeignOriginLocation":
+                out["CZForeignOriginLocation"] = self._parse_Location(child)
+            elif child.tag == "CZForeignDestinationLocation":
+                out["ForeignDestinationLocation"] = self._parse_Location(child)
+
+        if not out:
+            return None
+        return out
+
+    @staticmethod
+    def _parse_Identifiers(xml_root: ET.Element) -> dict:
+        out = dict()
+        for child in xml_root:
+            if child.tag == "PlannedTransportIdentifiers":
+                if "PlannedTransportIdentifiers" not in out:
+                    out["PlannedTransportIdentifiers"] = dict()
+                key, val = Parser._parse_TransportIdentifiers(child)
+                out["PlannedTransportIdentifiers"][key] = val
+            elif child.tag == "RelatedPlannedTransportIdentifiers":
+                assert "RelatedPlannedTransportIdentifiers" not in out
+                out["RelatedPlannedTransportIdentifiers"] = dict()
+                key, val = Parser._parse_TransportIdentifiers(child)
+                out["RelatedPlannedTransportIdentifiers"][key] = val
+
+        return out
+
+    @staticmethod
+    def _parse_TransportIdentifiers(xml_root: ET.Element) -> dict:
+        out = dict()
+        o_type = None
+        for child in xml_root:
+            if child.tag == "ObjectType":
+                o_type = child.text
+            elif child.tag == "Company":
+                out["Company"] = int(child.text)
+            elif child.tag == "Core":
+                out["Core"] = child.text
+            elif child.tag == "Variant":
+                out["Variant"] = child.text
+            elif child.tag == "TimetableYear":
+                out["TimetableYear"] = int(child.text)
+
+        return o_type, out
+
+    def _parse_CZPTTInformation(self, xml_root: ET.Element) -> dict:
+        out = dict()
+        for child in xml_root:
+            if child.tag == "CZPTTLocation":
+                if "CZPTTLocation" not in out:
+                    out["CZPTTLocation"] = list()
+                out["CZPTTLocation"].append(self._parse_CZPTTLocation(child))
+            elif child.tag == "PlannedCalendar":
+                out["PlannedCalendar"] = self._parse_PlannedCalendar(child)
+                out["PlannedCalendar"]["Canceled"] = list()
+
+        return out
+
+    def _parse_CZPTTLocation(self, xml_root: ET.Element) -> dict:
+        out = dict()
+        for child in xml_root:
+            if child.tag == "Location":
+                out["Location"] = self._parse_Location(child)
+            elif child.tag == "TimingAtLocation":
+                out["TimingAtLocation"] = self._parse_TimingAtLocation(child)
+            elif child.tag == "ResponsibleRU":
+                out["ResponsibleRU"] = int(child.text)
+            elif child.tag == "ResponsibleIM":
+                out["ResponsibleIM"] = int(child.text)
+            elif child.tag == "TrainType":
+                out["TrainType"] = int(child.text)
+            elif child.tag == "TrafficType":
+                out["TrafficType"] = child.text
+            elif child.tag == "CommercialTrafficType":
+                out["CommercialTrafficType"] = int(child.text)
+            elif child.tag == "TrainActivity":
+                out["TrainActivity"] = child.find("TrainActivityType").text
+            elif child.tag == "OperationalTrainNumber":
+                out["OperationalTrainNumber"] = int(child.text)
+            elif child.tag == "NetworkSpecificParameter":
+                if "NetworkSpecificParameter" not in out:
+                    out["NetworkSpecificParameter"] = list()
+                out["NetworkSpecificParameter"].append(self._parse_NetworkSpecificParameter(child))
+
+        return out
+
+    def _parse_Location(self, xml_root: ET.Element) -> dict:
+        data = dict()
+        data["LocationSubsidiaryIdentification"] = None
+        for child in xml_root:
+            if child.tag == "CountryCodeISO":
+                data["CountryCodeISO"] = child.text
+            elif child.tag == "LocationPrimaryCode":
+                data["LocationPrimaryCode"] = int(child.text)
+            elif child.tag == "PrimaryLocationName":
+                data["PrimaryLocationName"] = child.text
+            elif child.tag == "LocationSubsidiaryIdentification":
+                data["LocationSubsidiaryIdentification"] = self._parse_LocationSubsidiaryIdentification(child)
+
+        name = data["PrimaryLocationName"]
+        code = data["LocationPrimaryCode"]
+        track = data["LocationSubsidiaryIdentification"]["LocationSubsidiaryCode"] if data["LocationSubsidiaryIdentification"] is not None else None
+        if (name, code, track) not in self.Locations_dict:
+            self.Locations_dict[(name, code, track)] = data
+
+        placeholder = (name, code, track)
+
+        return {"Location_id": placeholder, "Name": name}
+
+    @staticmethod
+    def _parse_PlannedCalendar(xml_root: ET.Element) -> dict:
+        data = dict()
+        for child in xml_root:
+            if child.tag == "BitmapDays":
+                data["BitmapDays"] = [x == "1" for x in child.text]
+            elif child.tag == "ValidityPeriod":
+                data["ValidityPeriod"] = Parser._parse_ValidityPeriod(child)
+
+        d: datetime = data["ValidityPeriod"]["StartDateTime"]
+        de = data["ValidityPeriod"]["EndDateTime"]
+
+        out = dict()
+        out["ValidityPeriod"] = data["ValidityPeriod"]
+        out["Valid"] = list()
+        i = 0
+        while d <= de:
+            if data["BitmapDays"][i]:
+                out["Valid"].append(d.strftime("%Y%m%d"))
+
+            d += timedelta(days=1)
+            i += 1
+
+        return out
+
+    @staticmethod
+    def _parse_NetworkSpecificParameter(xml_root: ET.Element) -> dict:
+        out = dict()
+        for child in xml_root:
+            if child.tag == "Name":
+                out["Name"] = child.text
+            elif child.tag == "Value":
+                out["Value"] = child.text
+
+        return out
+
+    def _parse_CZDeactivatedSection(self, xml_root: ET.Element) -> dict:
+        out = dict()
+        for child in xml_root:
+            if child.tag == "StartLocation":
+                out["StartLocation"] = self._parse_Location(child)
+            elif child.tag == "EndLocation":
+                out["EndLocation"] = self._parse_Location(child)
+
+        return out
+
+    @staticmethod
+    def _parse_ValidityPeriod(xml_root: ET.Element) -> dict:
+        out = dict()
+        for child in xml_root:
+            if child.tag == "StartDateTime":
+                out["StartDateTime"] = Parser._parse_date_time(child.text)
+            elif child.tag == "EndDateTime":
+                out["EndDateTime"] = Parser._parse_date_time(child.text)
+        return out
+
+    @staticmethod
+    def _parse_LocationSubsidiaryIdentification(xml_root: ET.Element) -> dict:
+        out = dict()
+        for child in xml_root:
+            if child.tag == "LocationSubsidiaryCode":
+                out["LocationSubsidiaryCode"] = child.text
+                out["LocationSubsidiaryTypeCode"] = int(child.attrib["LocationSubsidiaryTypeCode"])
+            elif child.tag == "AllocationCompany":
+                out["AllocationCompany"] = child.text
+            elif child.tag == "LocationSubsidiaryName":
+                out["LocationSubsidiaryName"] = child.text
+
+        return out
+
+    @staticmethod
+    def _parse_TimingAtLocation(xml_root: ET.Element) -> dict:
+        out = dict()
+        for child in xml_root:
+            if child.tag == "Timing" and child.attrib["TimingQualifierCode"] == "ALA":
+                out["ALA"] = Parser._parse_Timing(child)
+            elif child.tag == "Timing" and child.attrib["TimingQualifierCode"] == "ALD":
+                out["ALD"] = Parser._parse_Timing(child)
+            elif child.tag == "DwellTime":
+                out["DwellTime"] = int(float(child.text)*60)
+
+        return out
+
+    @staticmethod
+    def _parse_Timing(xml_root: ET.Element) -> dict:
+        out = dict()
+        for child in xml_root:
+            if child.tag == "Time":
+                out["Time"] = datetime.strptime(child.text[:8], "%H:%M:%S")
+            elif child.tag == "Offset":
+                out["Offset"] = int(child.text)
+
+        return out
+
+    @staticmethod
+    def _parse_date_time(string: str) -> int:
+        dt = datetime.strptime(string, "%Y-%m-%dT%H:%M:%S")
+        return dt
