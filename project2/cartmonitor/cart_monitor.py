1f40b4db85edbfcacf6c466e04214a4758ac5e76
koniksedy <xsedym02@vutbr.cz>
2023-04-18 17:16:52 +0200

    commented + requests generator created

--- project2/cartmonitor/cart_monitor.py
+++ project2/cartmonitor/cart_monitor.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 """
 Dynamic analyser of a cart controller.
+Project 2 (ATA)
+Author: Michal Šedý <xsedym02@vutbr.cz>
 """
 
 class Singleton(type):
+    """Implementation of a singleton metaclass."""
     _instances = {}
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
+
 class Monitor(metaclass=Singleton):
+    """Dynamic analyser (singleton)
+
+    Atributes:
+        CART_STATIONS (set): A set of station names.
+        CART_MAX_SLOTS (int): A number of cart's slots.
+        CART_MAX_WEIGHT (int): A maximum cart weight (load).
+    """
     __metaclass__ = Singleton
 
     CART_STATIONS = {"A", "B", "C", "D"}
@@ -18,6 +29,10 @@ class Monitor(metaclass=Singleton):
     CART_MAX_WEIGHT = 150
 
     class No2LoadsOrUnloadsAtm:
+        """Class of the automaton for the monitoring of properties 1 and 2.
+        Property 1: The loading must not be applied on an occupied slot.
+        Property 2: The unloading must not be applid on an empty slot.
+        """
         def __init__(self, slot_id: int) -> None:
             self.slot_id = slot_id
             self.state = "empty"
@@ -27,20 +42,28 @@ class Monitor(metaclass=Singleton):
                                    "loading": "overflow"}}
 
         def step(self, event_time: str, event_type: str) -> None:
+            """Performs automaton step."""
             try:
                 self.state = self.trans[self.state][event_type]
             except KeyError:
-                print(f"{event_time}:error: {event_type} on slot #{self.slot_id} in the state {self.state} is not possible")
+                # No suitable transition.
+                print(f"{event_time}:error: other; {event_type} on slot #{self.slot_id} in the state {self.state} is not possible")
                 return
 
             if self.state == "underflow":
-                print(f"{event_time}:error: unloading from an empty slot #{self.slot_id}")
+                # Property 2 violation
+                print(f"{event_time}:error: property 2 violation; unloading from an empty slot #{self.slot_id}")
                 self.state = "empty"
             elif self.state == "overflow":
-                print(f"{event_time}:error: loading into an occupied slot #{self.slot_id}")
+                # Property 1 violation
+                print(f"{event_time}:error: property 1 violation; loading into an occupied slot #{self.slot_id}")
                 self.state = "full"
 
     class MustLoadAndUnloadAtm:
+        """Class of the automaton for the monitoring of properties 4 and 8.
+        Property 4: Every request must cause loading at some time.
+        Property 8: Every loading must cause unloading at some time.
+        """
         def __init__(self, item_name: str) -> None:
             self.cargo_name = item_name
             self.state = "created"
@@ -50,31 +73,42 @@ class Monitor(metaclass=Singleton):
                                      "stop": "not_unloaded"}}
 
         def step(self, event_time: str, event_type: str) -> None:
+            """Performs automaton step."""
             try:
                 self.state = self.trans[self.state][event_type]
             except KeyError:
-                print(f"{event_time}:error: {event_type} on cargo {self.cargo_name} in the state {self.state} is not possible")
+                # No suitable transition.
+                print(f"{event_time}:error: other; {event_type} on cargo {self.cargo_name} in the state {self.state} is not possible")
                 return
 
             if self.state == "not_unloaded":
-                print(f"{event_time}:error: a cargo {self.cargo_name} was not unloaded before the cart stop")
+                # Property 8 violation.
+                print(f"{event_time}:error: property 8 violation; a cargo {self.cargo_name} was not unloaded before the cart stop")
             elif self.state == "not_loaded":
-                print(f"{event_time}:error: a cargo {self.cargo_name} was not loaded before the cart stop")
+                # Property 4 violoation.
+                print(f"{event_time}:error: property 4 violation; a cargo {self.cargo_name} was not loaded before the cart stop")
 
     class CartLimitAtm:
+        """Class of the automaton for the monitoring of properties 6 and 7.
+        Property 6: A cart must not contain more than 4 cargo.
+        Property 7: A cart must not be overloaded.
+        """
         def __init__(self) -> None:
             self.cargo_cnt = 0
             self.cargo_weight = 0
 
         def step(self, event_time: str, event_type: str, weight: str) -> None:
+            """Performs automaton step."""
             if event_type == "loading":
                 self.cargo_weight += int(weight)
                 self.cargo_cnt += 1
 
                 if self.cargo_cnt > Monitor.CART_MAX_SLOTS:
-                    print(f"{event_time}:error: loaded more than 4 cargo on the cart")
+                    # Property 6 violation.
+                    print(f"{event_time}:error: property 6 violation; loaded more than 4 cargo on the cart; contains {self.cargo_cnt} cargo")
                 if self.cargo_weight > Monitor.CART_MAX_WEIGHT:
-                    print(f"{event_time}:error: the cart is overloaded; current weight is {self.cargo_weight}")
+                    # Property 7 violation.
+                    print(f"{event_time}:error: property 7 violation; the cart is overloaded; current weight is {self.cargo_weight}")
 
             elif event_type == "unloading":
                 if self.cargo_cnt != 0:
@@ -82,12 +116,16 @@ class Monitor(metaclass=Singleton):
                     self.cargo_cnt -= 1
 
     class DirectUnloadAtm:
+        """Class of the automaton for the monitoring of the property 3.
+        Property 3: A cargo must be unloaded, when the cart is at the destination.
+        """
         def __init__(self, cargo_name: str, dst: str) -> None:
             self.cargo_dst = dst
             self.cargo_name = cargo_name
             self.state = "created"
             self.trans = {"created": {("moving", pos): "created" for pos in Monitor.CART_STATIONS}}
             self.trans["created"]["loading"] = "loaded"
+            self.trans["loaded"] = dict()
             self.trans["loaded"][("moving", dst)] = "late"
             self.trans["loaded"][("unloading", dst)] = "unloaded"
             for pos in Monitor.CART_STATIONS.difference(dst):
@@ -95,18 +133,25 @@ class Monitor(metaclass=Singleton):
                 self.trans["loaded"][("moving", pos)] = "loaded"
 
         def step(self, event_time: str, event_type: str) -> None:
+            """Performs automaton step."""
             try:
                 self.state = self.trans[self.state][event_type]
             except KeyError:
-                print(f"{event_time}:DUerror: {event_type} on cargo {self.cargo_name} in the state {self.state} is not possible")
+                # No suitable transition.
+                print(f"{event_time}:error: other; {event_type} on cargo {self.cargo_name} in the state {self.state} is not possible")
                 return
 
             if self.state == "late":
-                print(f"{event_time}:error: {self.cargo_name} was not unloaded on the destination station")
+                # Property 3 violation - not unloaded at a destination.
+                print(f"{event_time}:error: property 3 violation; {self.cargo_name} was not unloaded at the destination station")
             if self.state == "bad_unloading":
-                print(f"{event_time}:error: {self.cargo_name} was not supposed to be unloade on {event_type[1]}")
+                # Property 3 violation - unloaded out of a destination.
+                print(f"{event_time}:error: property 3 violation; {self.cargo_name} was not supposed to be unloade on {event_type[1]}")
 
     class RequestBeforeLoadAtm:
+        """Class of the automaton for the monitoring of the property 5.
+        Property 5: A cart must not load in the station if there is no request.
+        """
         def __init__(self, cargo_name: str, src: str) -> None:
             self.cargo_src = src
             self.cargo_name = cargo_name
@@ -119,23 +164,30 @@ class Monitor(metaclass=Singleton):
             try:
                 self.state = self.trans[self.state][event_type]
             except KeyError:
-                print(f"{event_time}:RBerror: {event_type} on cargo {self.cargo_name} in the state {self.state} is not possible")
+                # No suitable transition.
+                print(f"{event_time}:error: other; {event_type} on cargo {self.cargo_name} in the state {self.state} is not possible")
                 return
 
             if self.state == "bad_loading":
-                print(f"{event_time}:error: no priorir request for the loading of {self.cargo_name} at {self.cargo_src}")
+                # Property 5 violation.
+                print(f"{event_time}:error: property 5 violation; no priorir request for the loading of {self.cargo_name} at {self.cargo_src}")
 
     class NoIdleWhenRequestAtm:
+        """Class of the automaton for the monitoring of the property 9.
+        Property 9: The cart mast not be in the mode idle when some reqest exists.
+        """
         def __init__(self) -> None:
             self.request_cnt = 0
             self.idle_time = None
 
         def step(self, event_time: str, event_type: str) -> int:
+            """Performs an automaton step."""
             if self.idle_time is not None:
                 if self.idle_time == int(event_time) and event_type != "idle":
                     self.idle_time = None
                 else:
-                    print(f"{event_time}:error: the cart is in the idle mode even with pending transportation request(s)")
+                    # Property 9 violation.
+                    print(f"{event_time}:error: property 9 violation; the cart is in the idle mode with pending transportation request(s)")
 
             if event_type == "requesting":
                 self.request_cnt += 1
@@ -146,6 +198,8 @@ class Monitor(metaclass=Singleton):
 
 
     def __init__(self) -> None:
+        self.last_event_time = 0
+
         # The set containing unique pairs of loading source station and slot of a cart.
         self.uniq_loads = set()
 
@@ -171,19 +225,24 @@ class Monitor(metaclass=Singleton):
 
         # A dictionary of automata for each cargo, controlling cargo loading and unloading.
         # Indexed by cargo name.
-        # The cargo must be loaded in some time if the transport reques has been made. (property 4)
-        # The loaded cargu must be unloaded in some time. (property 8)
+        # Every request must cause loading at some time. (property 4)
+        # Every loading must cause unloading at some time. (property 8)
         self.must_load_and_unload_dict: dict[str, Monitor.MustLoadAndUnloadAtm] = dict()
 
         # The cart mast not be in the mode idle when some reqest exists. (property 9)
         self.no_idle_when_request_atm = Monitor.NoIdleWhenRequestAtm()
 
     def onevent(self, event) -> None:
+        """Event handler."""
         event_time = event[0]
+        self.last_event_time = event_time
         event_id = event[1]
         del event[1]
 
+        # Testing property 9.
         self.no_idle_when_request_atm.step(event_time, event_id)
+
+        # Handling event.
         if event_id == "moving":
             self.onmoving(*event)
         elif event_id == "requesting":
@@ -192,79 +251,97 @@ class Monitor(metaclass=Singleton):
             self.onloading(*event)
         elif event_id == "unloading":
             self.onunloading(*event)
-        elif event_id == "stop":
-            self.onstop(*event)
 
     def onmoving(self, time: str, src: str, dst: str) -> None:
+        """moving handler"""
         for atm in self.direct_unload_dict.values():
-            try:
-                atm.step(time, ("moving", src))
-            except KeyError:
-                print(f"{time}:error: A cargo {atm.cargo_name} can not be moved. It is in the state {atm.state}.")
+            atm.step(time, ("moving", src))
 
     def onrequesting(self, time: str, src: str, dst: str, cargo: str, weight: str) -> None:
+        """requesting handler
+        New automata ned to be instantiated.
+        """
         if cargo in self.request_before_load_dict or cargo in self.must_load_and_unload_dict or cargo in self.direct_unload_dict:
-            print(f"{time}:error: the request for {cargo} has been allready made")
+            # This cargo allready exists.
+            print(f"{time}:error: other; the request for {cargo} has been allready made")
+
         self.request_before_load_dict[cargo] = Monitor.RequestBeforeLoadAtm(cargo, src)
         self.must_load_and_unload_dict[cargo] = Monitor.MustLoadAndUnloadAtm(cargo)
         self.direct_unload_dict[cargo] = Monitor.DirectUnloadAtm(cargo, dst)
 
 
     def onloading(self, time: str, pos: str, cargo: str, weight: str, slot: str) -> None:
+        """loading handler"""
+
+        # Remembering the load for the coverage computation.
         self.uniq_loads.add((pos, slot))
 
+        # Testing properties 1 and 2.
         self.no_2_loads_or_unloads_list[int(slot)].step(time, "loading")
+
+        # Testing properties 6 and 7.
         self.cart_limit_atm.step(time, "loading", weight)
 
         if cargo not in self.request_before_load_dict or cargo not in self.must_load_and_unload_dict or cargo not in self.direct_unload_dict:
-            print(f"{time}:error: The cargo {cargo} can not be loaded. It does not exist.")
+            # The loaded cargo does not exist.
+            print(f"{time}:error: other; The cargo {cargo} can not be loaded. It does not exist.")
 
+        # Testing propery 3.
         if cargo in self.direct_unload_dict:
             self.direct_unload_dict[cargo].step(time, "loading")
+
+        # Testing property 5.
         if cargo in self.request_before_load_dict:
             self.request_before_load_dict[cargo].step(time, ("loading", pos))
+            # Removing automaton.
             del self.request_before_load_dict[cargo]
+
+        # Testing properties 4 and 8.
         if cargo in self.must_load_and_unload_dict:
             self.must_load_and_unload_dict[cargo].step(time, "loading")
 
     def onunloading(self, time: str, pos: str, cargo: str, weight: str, slot: str) -> None:
+        """unloading handler"""
+
+        # Testing properties 1 and 2.
         self.no_2_loads_or_unloads_list[int(slot)].step(time, "unloading")
         self.cart_limit_atm.step(time, "unloading", weight)
 
         if cargo not in self.must_load_and_unload_dict or cargo not in self.direct_unload_dict:
-            print(f"{time}:error: The cargo {cargo} can not be unloaded. It does not exist.")
+            # The unloaded cargo des not exist.
+            print(f"{time}:error: other; The cargo {cargo} can not be unloaded. It does not exist.")
 
+        # Testing propery 3.
         if cargo in self.direct_unload_dict:
-            self.direct_unload_dict[cargo].step(time, "unloading")
+            self.direct_unload_dict[cargo].step(time, ("unloading", pos))
+            # Removing automaton.
             del self.direct_unload_dict[cargo]
+
+        # Testing properties 4 and 8.
         if cargo in self.must_load_and_unload_dict:
             self.must_load_and_unload_dict[cargo].step(time, "unloading")
+            # Removing automaton.
             del self.must_load_and_unload_dict[cargo]
 
-    def onstop(self, time: str) -> None:
+    def onfinish(self) -> None:
+        """finish handler"""
+
+        # Testing properties 4 and 8.
         for atm in self.must_load_and_unload_dict.values():
-            atm.step(time, "stop")
+            atm.step(self.last_event_time, "stop")
 
     def get_coverage(self) -> float:
+        """Calculates the cart coverage."""
         return len(self.uniq_loads)/(len(Monitor.CART_STATIONS)*Monitor.CART_MAX_SLOTS)
 
 
 def report_coverage():
     """Coverage reporter"""
-    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # Zde nahradte vypocet/vypis aktualne dosazeneho pokryti
-    print('CartCoverage %d%%' % ((Monitor().get_coverage())*100))
-
-def onmoving(time, pos1, pos2):
-    """priklad event-handleru pro udalost moving"""
-    # Podobnou funkci muzete i nemusite vyuzit, viz onevent().
-    # Vsechny parametry jsou typu str; nektere muze byt nutne pretypovat.
-    time = int(time)
-    print('%d:debug: got moving from %s to %s' % (time, pos1, pos2))
+    print("CartCoverage %d%%" % ((Monitor().get_coverage())*100))
 
 def onevent(event):
     """Event handler. event = [TIME, EVENT_ID, ...]"""
-    print(event)
+    # print(event)
     Monitor().onevent(event)
 
 ###########################################################
@@ -275,10 +352,9 @@ def monitor(reader):
     for line in reader:
         line = line.strip()
         onevent(line.split())
+    Monitor().onfinish()
     report_coverage()
 
 if __name__ == "__main__":
     import sys
     monitor(sys.stdin)
-    # with open("bad_log.txt", "r") as fd:
-    #     monitor(fd)
